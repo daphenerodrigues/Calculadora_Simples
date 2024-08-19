4fe2e0313e58800b51365f364e19900a4ac17f6b
daphenerodrigues <daphene.rodrigues@outlook.com>
2024-08-19 16:44:57 -0400

    Update Calculadora_Simples.py

--- Calculadora_Simples.py
+++ Calculadora_Simples.py
@@ -1,7 +1,19 @@
-echo "# Calculadora_Simples" >> README.md 
-git init 
-git add README.md 
-git commit -m "primeiro commit" 
-git branch -M main 
-git remote add origin https://github.com/daphenerodrigues/Calculadora_Simples.git
- git push -u origin main
+print("Bem vindo(a) à Calculadora Simples da Daphene")
+numeroa = float(input("Insira o primeiro número: "))
+operacao = input("Insira a operação que deseja: ")
+numerob = float(input("Insira o segundo número: "))
+
+if operacao == "+":
+    resultado = numeroa + numerob
+    print("O resultado é: ", resultado)
+elif operacao == "-":
+    resultado = numeroa - numerob
+    print("O resultado é: ", resultado)
+elif operacao == "/":
+    resultado = numeroa / numerob
+    print("O resultado é: ", resultado)
+elif operacao == "*":
+    resultado = numeroa * numerob
+    print("O resultado é: ", resultado)
+else: 
+    print("Operação inválida")
